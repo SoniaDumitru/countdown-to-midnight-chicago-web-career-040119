bd2c03516690048829ef22d27989edb602ed9e7f
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-12 06:09:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
-
-def countdown
+  def countdown(integer)
+  while integer > 0
+    puts "#{integer} SECONDS(S)"
+    sleep 1
+    integer -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
